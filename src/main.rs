d20cb45108eb53209da771069ecf601204309100
Mohamed Ali Gosrani <m84677@eon.com>
2023-09-15 13:06:21 +0200

    wip: Node Labels

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::collections::BTreeMap;
 
+use serde::{Deserialize, Serialize};
+
 // hide console window on Windows in release
-use eframe::egui::{self};
-use egui_graphs::{to_input_graph, Graph, GraphView, SettingsInteraction};
+use eframe::egui::{self, Vec2};
+use egui_graphs::{default_edge_transform, Graph, GraphView, Node, SettingsInteraction};
 use gitlab::{
     api::{groups::GroupBuilder, projects::repository::TreeBuilder, ApiError, Query},
     Project,
 };
-use petgraph::{stable_graph::StableGraph, Directed};
-
+use petgraph::{prelude::*, EdgeType};
 mod gitlab_group;
 use gitlab_group::Group;
+pub const DEFAULT_SPAWN_SIZE: f32 = 250.;
 
 fn main() -> Result<(), eframe::Error> {
     if std::env::var("RUST_LOG").is_err() {
@@ -35,35 +37,86 @@ struct ConfigAnalyzer {
     password: String,
     gitlab_client: Option<gitlab::Gitlab>,
     data: BTreeMap<Box<str>, Project>,
-    g: Option<Graph<(), (), Directed>>,
+    graph: Option<Graph<ProjectNode, (), Directed>>,
+}
+
+#[derive(Clone, Hash, std::fmt::Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+struct ProjectNode {
+    id: Box<str>,
+    name: Box<str>,
+    group: Box<str>,
+}
+impl From<Project> for ProjectNode {
+    fn from(value: Project) -> Self {
+        Self {
+            id: value.id.to_string().into_box_str(),
+            name: value.name.into_box_str(),
+            group: value.namespace.name.into_box_str(),
+        }
+    }
 }
 
 impl ConfigAnalyzer {
     fn generate_graph(&mut self) {
         // TODO implement dependency graph
-        let mut g: StableGraph<(), ()> = StableGraph::new();
+        // let mut g: DiGraphMap<ProjectNode, ()> = GraphMap::new();
+        let mut g: StableGraph<ProjectNode, ()> = StableGraph::new();
+        let mut d: BTreeMap<Box<str>, usize> = BTreeMap::new();
 
-        let a = g.add_node(());
-        let b = g.add_node(());
-        let c = g.add_node(());
+        for project in self.data.iter() {
+            // let node = g.add_node(());
+            // let node = Node::default();
+            // let node = node.with_data(Some(project.1));
+            let index = g.add_node(project.1.clone());
+            d.insert(project.0.clone(), index.index());
+        }
+        // let a = g.add_node(());
+        // let b = g.add_node(());
+        // let c = g.add_node(());
 
-        g.add_edge(a, b, ());
-        g.add_edge(b, c, ());
-        g.add_edge(c, a, ());
+        // g.add_edge(a, b, ());
+        // g.add_edge(b, c, ());
+        // g.add_edge(c, a, ());
 
-        self.g = Some(to_input_graph(&g))
+        self.graph = Some(to_input_graph(&g));
+        // println!("{:?}", Dot::with_config(&self.graph, &[Config::EdgeNoLabel]));
+        // graph {
+        //     0 [label="\"0\""]
+        //     1 [label="\"0\""]
+        //     2 [label="\"0\""]
+        //     3 [label="\"0\""]
+        //     1 -- 2
+        //     3 -- 4
+        //     2 -- 3
+        // }
     }
+}
+
+pub fn random_location(size: f32) -> Vec2 {
+    let mut rng = rand::thread_rng();
+    Vec2::new(rng.gen_range(0. ..size), rng.gen_range(0. ..size))
+}
+
+pub fn to_input_graph<N: Clone, E: Clone, Ty: EdgeType>(
+    g: &StableGraph<N, E, Ty>,
+) -> Graph<N, E, Ty> {
+    transform(g, projects_node_transform, default_edge_transform)
+}
+
+pub fn projects_node_transform<N: Clone>(idx: NodeIndex, data: &N) -> Node<N> {
+    let loc = random_location(DEFAULT_SPAWN_SIZE);
+    Node::new(loc, data.clone()).with_label(idx.index().to_string())
 }
 
 impl Default for ConfigAnalyzer {
     fn default() -> Self {
         Self {
-            url: "".to_owned(),
-            group: "".to_owned(),
+            url: "git.eon-cds.de/".to_owned(),
+            group: "23994".to_owned(),
             password: std::env::var("GITLAB_TOKEN").unwrap_or_default(),
             gitlab_client: None,
             data: BTreeMap::new(),
-            g: None,
+            graph: None,
         }
     }
 }
@@ -159,7 +212,7 @@ impl eframe::App for ConfigAnalyzer {
             });
         });
         egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
-            if let Some(mut graph) = self.g.as_mut() {
+            if let Some(mut graph) = self.graph.as_mut() {
                 let interaction_settings = &SettingsInteraction::new()
                     .with_dragging_enabled(true)
                     .with_clicking_enabled(true)
